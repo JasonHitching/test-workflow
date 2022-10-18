2f2bbc9b89c9fbfc28d2fed2c1c7021502e9a5d6
Jason Hitching <jason_hitching@tjxeurope.com>
2022-10-18 13:26:45 +0100

    testing

--- test/ArrayManipulator.spec.js
+++ test/ArrayManipulator.spec.js
@@ -4,7 +4,7 @@ const expect = require("chai").expect;
 describe("testing bogus array size", () => {
   it("should only have on item in the array when a single push executes", () => {
     const manipulator = new ArrayManipulator();
-
+    // hi
     manipulator.addItem("hi");
 
     expect(manipulator.bogusArr.length).to.eq(1);
