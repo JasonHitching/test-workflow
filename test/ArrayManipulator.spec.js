04effaf537eb02e707df6bfce1b04da6b9b2bb41
Jason Hitching <jason_hitching@tjxeurope.com>
2022-10-19 16:26:00 +0100

    some commit

--- test/ArrayManipulator.spec.js
+++ test/ArrayManipulator.spec.js
@@ -5,9 +5,12 @@ describe("testing bogus array size", () => {
   it("should only have on item in the array when a single push executes", () => {
     const manipulator = new ArrayManipulator();
 
-    // Item added
+    // Item added!
     manipulator.addItem("hi");
 
     expect(manipulator.bogusArr.length).to.eq(1);
   });
 });
+
+// git add . && git commit -m "some commit" && git push --set-upstream origin demo-branch
+// git add . && git commit -m "broke it" && git push
